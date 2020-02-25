c89842f62b270f1355bfcb02b1a49902ad2cb6ec
AliaksandraBychkouskaya <36661957+AliaksandraBychkouskaya@users.noreply.github.com>
2020-02-25 22:44:43 +0300

    Update Number12.java

--- src/by/tr/mod2/Number12.java
+++ src/by/tr/mod2/Number12.java
@@ -1,5 +1,8 @@
 package by.tr.mod2;
 
+import java.sql.SQLOutput;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Number12 {
@@ -7,9 +10,44 @@ public class Number12 {
         Scanner sc = new Scanner(System.in);
         System.out.print("Введите строку: ");
         String str = "";
+        
         if (sc.hasNextLine()) {
             str = sc.nextLine();
         }
-        System.out.println(str);
+        
+        String [] splittedStr = (str.split(""));
+        
+        for (int i = 0; i < splittedStr.length; i++) {
+            if (i == 0) {
+                
+                String character = splittedStr[i];
+                int ascii = (int) character.charAt(0);
+                
+                if (ascii == 95 || (ascii >= 65 && ascii <= 90) ||(ascii >= 97 && ascii <= 122))
+                {
+                    continue;
+                }
+                else {
+                    System.out.println("Первый символ не удовлетворяет требованию");
+                    break;
+                }
+            }
+            else {
+                
+                String character = splittedStr[i];
+                int ascii = (int) character.charAt(0);
+                
+                if (ascii == 95 || (ascii >= 65 && ascii <= 90) ||(ascii >= 97 && ascii <= 122) ||(ascii >= 48 && ascii <= 57))
+                {
+                    if (i == splittedStr.length-1)
+                    System.out.println("Строка удовлетворяет условию");
+                }
+                else {
+                    System.out.println("Строка не удовлетворяет условию");
+                    break;
+                }
+            }
+        }
     }
 }
+
